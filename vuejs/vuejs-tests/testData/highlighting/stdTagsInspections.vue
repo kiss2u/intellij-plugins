0186a24a41299925ad7aa5fb1c6434e7883f2514
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-02-12 09:30:06 +0100

    WEB-76794 HTML: February 2026 HTML5 schema update - update test data
    GitOrigin-RevId: 64e9dc72924c6ada282849abb3485c7a78f66148

--- vuejs/vuejs-tests/testData/highlighting/stdTagsInspections.vue
+++ vuejs/vuejs-tests/testData/highlighting/stdTagsInspections.vue
@@ -6,7 +6,7 @@
   <area></area>
   <!-- missing alt attribute -->
   <area></area>
-  <<warning descr="Element img doesn't have required attribute src"><warning descr="Missing required 'alt' attribute">img</warning></warning>><warning descr="Closing tag is redundant"></<warning descr="Element img doesn't have required attribute src">img</warning>></warning>
+  <<warning descr="Missing required 'alt' attribute">img</warning>><warning descr="Closing tag is redundant"></img></warning>
   <<warning descr="Missing required 'alt' attribute"><warning descr="Obsolete tag">applet</warning></warning>></<warning descr="Obsolete tag">applet</warning>>
 </template>
 <script>
